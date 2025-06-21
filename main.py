5539c0608ab277012ed93dca1f1387bdf0b9142e
Hoscar <oscarodriguez.05.18@gmail.com>
2025-06-21 13:35:32 -0400

    Memoria agregada. postdata: recuerda cambiar de rama cuando haga cambios

--- main.py
+++ main.py
@@ -18,16 +18,22 @@ from langchain_cohere import ChatCohere
 
 llm = ChatCohere()
 
+# Implementing memory
+from langgraph.checkpoint.memory import InMemorySaver
+
+checkpoint_saver = InMemorySaver()
+
 # Create Agent react
 from langgraph.prebuilt import create_react_agent
 
-agent = create_react_agent(model=llm, tools=[])
+agent = create_react_agent(model=llm, tools=[], checkpointer=checkpoint_saver)
 
 # BaseModel
 from pydantic import BaseModel
 
 class LLMRequest(BaseModel):
     promt: str
+    id: int
 
 # LLM Endpoint
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
@@ -35,6 +41,7 @@ from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 @app.post("/llm")
 async def llm_endpoint(request: LLMRequest):
+    config = {"configurable": {"thread_id": request.id}}
     user_prompt = HumanMessage(content=request.promt)
-    response = agent.invoke({"messages":user_prompt})
+    response = agent.invoke({"messages":user_prompt}, config=config)
     return {"response": response["messages"][-1].content}
